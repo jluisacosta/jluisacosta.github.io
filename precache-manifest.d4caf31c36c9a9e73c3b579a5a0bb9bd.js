f1f82a42d36c52395512878d7519d69b0fd9b990
José Luis Acosta <jluis.acosta@outlook.com>
2020-02-11 22:38:27 -0600

    Updates

---
+++ precache-manifest.d4caf31c36c9a9e73c3b579a5a0bb9bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fadc50b3cbe57231e51706f52686a57e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48482da3b002de637819",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3f98ad7fe3ec75534bd",
+    "url": "/static/js/2.aa3db6a6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.aa3db6a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48482da3b002de637819",
+    "url": "/static/js/main.6254c7da.chunk.js"
+  },
+  {
+    "revision": "2808d3bb67a3189d9fe8",
+    "url": "/static/js/runtime-main.a91e15f1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
