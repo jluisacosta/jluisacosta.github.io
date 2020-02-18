ac74381dfb72e542fa6884c6967e5ead00132d24
José Luis Acosta <jluis.acosta@outlook.com>
2020-02-17 23:21:31 -0600

    Set file structure, update extensions, create jsconfig for baseUrl

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from 'static/images/logo.svg';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          jluisacosta.github.io
-        </p>
+        <p>jluisacosta.github.io</p>
       </header>
     </div>
   );
