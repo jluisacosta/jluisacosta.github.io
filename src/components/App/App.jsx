d20c28511e0ea3f1a28f7fb46a4f0495f8555203
José Luis Acosta <jluis.acosta@outlook.com>
2020-02-21 13:21:47 -0600

    Update App component, remove uneeded setup files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import logo from 'static/images/logo.svg';
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>jluisacosta.github.io</p>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>jluisacosta.github.io</p>
+    </header>
+  </div>
+
+App.displayName = 'App'
 
 export default App;
