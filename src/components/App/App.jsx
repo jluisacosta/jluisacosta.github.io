cbd9213550fa50068d8fa38d75902538ddf178c8
José Luis Acosta <jluis.acosta@outlook.com>
2020-02-24 17:42:53 -0600

    Add fonts, styles order improvement

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,8 @@ const App = () =>
   <div className="App">
     <header className="App-header container">
       <img src={logo} className="App-logo" alt="logo" />
-      <p>jluisacosta.github.io</p>
+      <h3 className="App-title">José Luis Acosta</h3>
+      <p className="App-paragraph">jluisacosta.github.io</p>
       <i className="fas fa-code"/>
     </header>
   </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+import 'styles/index.scss';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'components/App';
-
-import 'styles/index.scss';
-import '@fortawesome/fontawesome-free/js/all'
 
 ReactDOM.render(<App />, document.getElementById('root'));
