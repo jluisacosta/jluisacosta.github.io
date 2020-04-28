a68b82db50c86aac40f0d1f8298d35ae4ba41e0f
José Luis Acosta <jluis.acosta@outlook.com>
2020-04-28 18:43:55 -0500

    Create Menu items

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -2,17 +2,27 @@ import React from 'react'
 
 import './Sidebar.scss'
 
+const MENU_ITEMS = [
+  ['fas fa-user', 'About Me'],
+  ['fas fa-code', 'Skills'],
+  ['fas fa-briefcase', 'Experience'],
+  ['fas fa-graduation-cap', 'Education'],
+  ['fas fa-envelope', 'Contact Me']
+]
+
 const Sidebar = () =>
   <nav className="Sidebar is-flex">
-    <button className="Sidebar-hamburgerButton">
+    <button className="Sidebar-hamburgerButton is-flex">
       <i className="fas fa-bars" />
     </button>
     <ul>
-      <li>About Me</li>
-      <li>Skills</li>
-      <li>Experience</li>
-      <li>Education</li>
-      <li>Contact Me</li>
+      {
+        MENU_ITEMS.map(([iconClass, description]) =>
+          <li className="Sidebar-menuItem is-flex">
+            <i className={iconClass} title={description}/>
+          </li>
+        )
+      }
     </ul>
     <button>Download Resumé</button>
   </nav>
