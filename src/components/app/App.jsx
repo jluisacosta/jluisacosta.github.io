7682e345e4fc325d075797a028d502cc1a77c6ee
José Luis Acosta <jluis.acosta@outlook.com>
2020-04-29 15:56:17 -0500

    Remove background component with floating squares

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
-import Background from '../background'
 import BaseLayout from '../base-layout'
 import Sidebar from '../sidebar'
 import ProfilePhoto from '../profile-photo'
 import Main from '../main'
 
+import './App.scss'
+
 const App = () =>
   <div className="App">
-    <Background>
-      <BaseLayout>
-        <Sidebar />
-        <ProfilePhoto />
-        <Main />
-      </BaseLayout>
-    </Background>
+    <BaseLayout>
+      <Sidebar />
+      <ProfilePhoto />
+      <Main />
+    </BaseLayout>
   </div>
 
 App.displayName = 'App'
