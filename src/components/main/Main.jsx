a4b3b5d89ea700b0ae0d0f2fe3ad73c0abf9ddc5
José Luis Acosta <jluis.acosta@outlook.com>
2020-05-01 19:42:44 -0500

    Implement transition handler for onSelect effect

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, {useState, useCallback, useRef, useEffect} from 'react'
 import PropTypes from 'prop-types'
+import classnames from 'classnames'
 import Section from '../section'
 
 import './Main.scss'
 
-const Main = ({activeSection}) =>
-  <div className="Main is-relative">
-    <Section activeSection={activeSection} />
-  </div>
+const TRANSITIONEND = 'transitionend'
+
+const Main = ({activeSection}) => {
+  const [className, setClassName] = useState('')
+  const resetClassName = useCallback(() => setClassName(''), [])
+  const mainRef = useRef(null)
+  
+
+  useEffect(() => {
+    let mainElement = mainRef.current
+    mainElement.addEventListener(TRANSITIONEND, resetClassName)
+    setClassName('isMounted')
+
+    return () => mainElement.removeEventListener(TRANSITIONEND, resetClassName)
+  }, [activeSection, resetClassName])
+
+  return (
+    <div className={classnames('Main is-relative', className)} ref={mainRef}>
+      <Section activeSection={activeSection} />
+    </div>
+  )
+}
 
 Main.displayName = 'Main'
 
