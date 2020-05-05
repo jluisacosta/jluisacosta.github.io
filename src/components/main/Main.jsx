ea6de0a39c7fcc29009820a054c72014d51b702f
José Luis Acosta <jluis.acosta@outlook.com>
2020-05-05 16:15:44 -0500

    Set sections placeholders in Main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import {SECTION_HEADERS} from 'utils'
+import {SECTIONS, SECTION_HEADERS} from 'utils'
 import SectionContainer from '../section-container'
 
 import './Main.scss'
@@ -9,7 +9,15 @@ import './Main.scss'
 const Main = ({className, activeSection, onTransitionEnd}) =>
   <div className={classnames('Main is-relative', className)} onTransitionEnd={onTransitionEnd}>
     <SectionContainer title={SECTION_HEADERS[activeSection]}>
-      Content
+      {
+        {
+          [SECTIONS.AboutMe]: <p>About Me Content</p>,
+          [SECTIONS.Skills]: <p>Skills Content</p>,
+          [SECTIONS.Experience]: <p>Experience Content</p>,
+          [SECTIONS.Education]: <p>Education Content</p>,
+          [SECTIONS.ContactMe]: <p>Contact Me Content</p>
+        }[activeSection]
+      }
     </SectionContainer>
   </div>
 
