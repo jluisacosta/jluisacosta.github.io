c95eccec5a100bb6c236a09c572442ba993e7806
José Luis Acosta <jluis.acosta@outlook.com>
2020-05-05 15:18:06 -0500

    Change section name and structure

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import Section from '../section'
+import {SECTION_HEADERS} from 'utils'
+import SectionContainer from '../section-container'
 
 import './Main.scss'
 
 const Main = ({className, activeSection, onTransitionEnd}) =>
   <div className={classnames('Main is-relative', className)} onTransitionEnd={onTransitionEnd}>
-    <Section activeSection={activeSection} />
+    <SectionContainer title={SECTION_HEADERS[activeSection]}>
+      Content
+    </SectionContainer>
   </div>
 
 Main.displayName = 'Main'
