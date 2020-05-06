9b0b8dd6532686ca14ad76b279f34adda120f4f6
José Luis Acosta <jluis.acosta@outlook.com>
2020-05-06 16:59:55 -0500

    Remove section container wrapper, move content to main component

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
-import SectionContainer from '../section-container'
 import {SECTIONS} from '../sections/index'
 
 import './Main.scss'
 
 const Main = ({className, activeSection, onTransitionEnd}) => {
+  const classNames = classnames('Main is-relative', className)
   const {title, view} = SECTIONS[activeSection]
 
   return (
-    <div className={classnames('Main is-relative', className)} onTransitionEnd={onTransitionEnd}>
-      <SectionContainer title={title.toLowerCase()}>{view}</SectionContainer>
+    <div className={classNames} onTransitionEnd={onTransitionEnd}>
+      <section className="Main-section">
+        <header className="Main-header">
+          <h1 className="Main-title">{title.toLowerCase()}</h1>
+        </header>
+        {view}
+      </section>
     </div>
   )
 }
